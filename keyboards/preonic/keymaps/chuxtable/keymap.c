ef0d5ec43883057c8eff33116710490844df347b
Chris Huxtable <chris@huxtable.ca>
2024-02-28 11:19:15 -0800

    Update keymaps

--- keyboards/preonic/keymaps/chuxtable/keymap.c
+++ keyboards/preonic/keymaps/chuxtable/keymap.c
@@ -24,6 +24,7 @@ enum preonic_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _WINDOW,
   _ADJUST
 };
 
@@ -31,8 +32,26 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  WINDOW,
+  LN_HOME,
+  LN_END,
   SLP_DSP,
-  LCK_DSP
+  LCK_DSP,
+  W_1L3,
+  W_1C3,
+  W_1R3,
+  W_2L3,
+  W_2R3,
+  W_1L2,
+  W_1R2,
+  W_QTL,
+  W_QTR,
+  W_QBL,
+  W_QBR,
+  W_MVL,
+  W_MVR,
+  W_CTR,
+  W_RST
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left |  Up  | Down |Right |
+ * |Window| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left |  Up  | Down |Right |
  * `-----------------------------------------------------------------------------------'
  *
  * ,-----------------------------------------------------------------------------------.
@@ -59,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   <  |   >  |   ?  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left |  Up  | Down |Right |
+ * |Window| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left |  Up  | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
@@ -67,16 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+  WINDOW,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |Sleep |      |      |      |      |      |      |      |      |      |      | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  | Home |      |  \   |  |   |  -   |   _  |   |  |   /  |      | End  | Del  |
+ * | Tab  | Home |LnHome|  \   |  |   |  -   |   _  |   |  |   /  |LnEnd | End  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  | Pg Up|  [   |  (   |  {   |  <   |   >  |   }  |   )  |   ]  | Pg Dn|  |   |
+ * |   ~  | Pg Up|      |  (   |  {   |  [   |   ]  |   }  |   )  |      | Pg Dn|  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |  +   |  -   |   _  |   =  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -85,33 +104,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid(
   SLP_DSP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
-  KC_TAB,  KC_HOME, XXXXXXX, KC_BSLS, KC_PIPE, KC_PMNS, KC_UNDS, KC_PIPE, KC_PSLS, XXXXXXX, KC_END,  KC_DEL,
-  KC_TILD, KC_PGUP, KC_LABK, KC_LPRN, KC_LCBR, KC_LBRC, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, KC_PGDN, KC_PIPE,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_PPLS, KC_PMNS, KC_UNDS, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
+  KC_TAB,  KC_HOME, LN_HOME, KC_BSLS, KC_PIPE, KC_PMNS, KC_UNDS, KC_PIPE, KC_PSLS, LN_END,  KC_END,  KC_DEL,
+  KC_TILD, KC_PGUP, XXXXXXX, KC_LPRN, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_RPRN, XXXXXXX, KC_PGDN, KC_BSLS,
+  _______, XXXXXXX, XXXXXXX, KC_LABK, KC_PPLS, KC_PMNS, KC_UNDS, KC_EQL,  KC_RABK, XXXXXXX, XXXXXXX, _______,
+  XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * | LkDp |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  | Home |      |  \   |  |   |  -   |   _  |   |  |   /  |      | End  | Del  |
+ * | Tab  | Home |LnHome|  \   |  |   |  -   |   _  |   |  |   /  |LnEnd | End  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  | Pg Up|  [   |  (   |  {   |  <   |   >  |   }  |   )  |   ]  | Pg Dn|  \   |
+ * |   ~  | Pg Up|      |  (   |  {   |  [   |   ]  |   }  |   )  |      | Pg Dn|  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |  +   |  -   |   _  |   =  |      |      |      |      |
+ * |      |      |      |  <   |  +   |  -   |   _  |   =  |   >  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol+ | Vol- | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
   LCK_DSP,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_TAB,  KC_HOME, XXXXXXX, KC_BSLS, KC_PIPE, KC_PMNS, KC_UNDS, KC_PIPE, KC_PSLS, XXXXXXX, KC_END,  KC_DEL,
-  KC_TILD, KC_PGUP, KC_LABK, KC_LPRN, KC_LCBR, KC_LBRC, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, KC_PGDN, KC_BSLS,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_PPLS, KC_PMNS, KC_UNDS, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
+  KC_TAB,  KC_HOME, LN_HOME, KC_BSLS, KC_PIPE, KC_PMNS, KC_UNDS, KC_PIPE, KC_PSLS, LN_END,  KC_END,  KC_DEL,
+  KC_TILD, KC_PGUP, XXXXXXX, KC_LPRN, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_RPRN, XXXXXXX, KC_PGDN, KC_BSLS,
+  _______, XXXXXXX, XXXXXXX, KC_LABK, KC_PPLS, KC_PMNS, KC_UNDS, KC_EQL,  KC_RABK, XXXXXXX, XXXXXXX, _______,
+  XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
 
 ),
+
+ /* Window
+  * ,-----------------------------------------------------------------------------------.
+  * | LkDp |      |      |      |      |      |      |      |      |      |      | W_RST|
+  * |------+------+------+------+------+------+------+------+------+------+------+------|
+  * |      | W_1L2| W_CTR| W_1R2|      |      |      |      |      | W_QTL| W_QTR|      |
+  * |------+------+------+------+------+-------------+------+------+------+------+------|
+  * |      | W_2L3|      | W_2R3|      |      |      |      |      | W_QBL| W_QBR|      |
+  * |------+------+------+------+------+------|------+------+------+------+------+------|
+  * |      | W_1L3| W_1C3| W_1R3|      |      |      |      |      |      |      |      |
+  * |------+------+------+------+------+------+------+------+------+------+------+------|
+  * |      |      |      |      |      |             |      | W_MVL|      |      | W_MVR|
+  * `-----------------------------------------------------------------------------------'
+  */
+ [_WINDOW] = LAYOUT_preonic_grid(
+   LCK_DSP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   W_RST,
+   XXXXXXX,   W_1L2,   W_CTR,   W_1R2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   W_QTL,   W_QTR, XXXXXXX,
+   XXXXXXX,   W_2L3, XXXXXXX,   W_2R3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   W_QBL,   W_QBR, XXXXXXX,
+   _______,   W_1L3,   W_1C3,   W_1R3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+   _______, _______, _______, _______, _______, _______, _______, _______,   W_MVL, XXXXXXX, XXXXXXX,   W_MVR
+
+ ),
+
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -119,74 +161,286 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |RGBTog|  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|      |      |      |      |RGBMod|
+ * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|      |      |BLhue+|BLhue-|RGBMod|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|      |      |      |      |RGBBre|RGBNor|RGBRMod|
+ * |      |Voice-|Voice+|Mus on|MusOff|      |      |BLrnbw|BLtwnk|BLbrth|BLstat|RGBRMod|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      | Bklgt|
+ * |      |      |      |      |      |             |      |      |BL+   |BL-   |BL 1/0|
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, RGB_TOG, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX,  XXXXXXX, _______, RGB_MOD,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  XXXXXXX, XXXXXXX, _______, _______,  RGB_M_B, RGB_M_P, RGB_RMOD,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_RSFT
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX,  RGB_HUI, RGB_HUD, RGB_MOD,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  XXXXXXX, XXXXXXX, RGB_M_R, RGB_M_TW, RGB_M_B, RGB_M_P, RGB_RMOD,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,  RGB_VAI, RGB_VAD, RGB_TOG
 )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-        case QWERTY:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case SLP_DSP:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            register_code(KC_RCTL);
-            register_code(KC_POWER);
-          } else {
-            unregister_code(KC_RSFT);
-            unregister_code(KC_RCTL);
-            unregister_code(KC_POWER);
-          }
-          return false;
-          break;
-        case LCK_DSP:
-          if (record->event.pressed) {
-            register_code(KC_RCMD);
-            register_code(KC_RCTL);
-            register_code(KC_Q);
-          } else {
-            unregister_code(KC_RCMD);
-            unregister_code(KC_RCTL);
-            unregister_code(KC_Q);
-          }
-        return false;
-        break;
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
       }
-    return true;
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case WINDOW:
+      if (record->event.pressed) {
+        layer_on(_WINDOW);
+      } else {
+        layer_off(_WINDOW);
+      }
+      return false;
+      break;
+    case SLP_DSP:
+      if (record->event.pressed) {
+        register_code(KC_RSFT);
+        register_code(KC_RCTL);
+        register_code(KC_POWER);
+      } else {
+        unregister_code(KC_RSFT);
+        unregister_code(KC_RCTL);
+        unregister_code(KC_POWER);
+      }
+      return false;
+      break;
+    case LCK_DSP:
+      if (record->event.pressed) {
+        register_code(KC_RCMD);
+        register_code(KC_RCTL);
+        register_code(KC_Q);
+      } else {
+        unregister_code(KC_RCMD);
+        unregister_code(KC_RCTL);
+        unregister_code(KC_Q);
+      }
+      return false;
+      break;
+    case LN_HOME:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_A);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_A);
+      }
+      return false;
+      break;
+    case LN_END:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_E);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_E);
+      }
+      return false;
+      break;
+    case W_1L3:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_D);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_D);
+      }
+      return false;
+      break;
+    case W_1C3:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_F);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_F);
+      }
+      return false;
+      break;
+    case W_1R3:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_G);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_G);
+      }
+      return false;
+      break;
+    case W_2L3:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_E);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_E);
+      }
+      return false;
+      break;
+    case W_2R3:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_T);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_T);
+      }
+      return false;
+      break;
+    case W_1L2:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_LEFT);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_LEFT);
+      }
+      return false;
+      break;
+    case W_1R2:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_RGHT);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_RGHT);
+      }
+      return false;
+      break;
+    case W_QTL:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_U);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_U);
+      }
+      return false;
+      break;
+    case W_QTR:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_I);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_I);
+      }
+      return false;
+      break;
+    case W_QBL:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_J);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_J);
+      }
+      return false;
+      break;
+    case W_QBR:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_K);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_K);
+      }
+      return false;
+      break;
+    case W_MVL:
+      if (record->event.pressed) {
+        register_code(KC_RCMD);
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_LEFT);
+      } else {
+        unregister_code(KC_RCMD);
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_LEFT);
+      }
+      return false;
+      break;
+    case W_MVR:
+      if (record->event.pressed) {
+        register_code(KC_RCMD);
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_RGHT);
+      } else {
+        unregister_code(KC_RCMD);
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_RGHT);
+      }
+      return false;
+      break;
+    case W_CTR:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_C);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_C);
+      }
+      return false;
+      break;
+    case W_RST:
+      if (record->event.pressed) {
+        register_code(KC_RCTL);
+        register_code(KC_ROPT);
+        register_code(KC_BSPC);
+      } else {
+        unregister_code(KC_RCTL);
+        unregister_code(KC_ROPT);
+        unregister_code(KC_BSPC);
+      }
+      return false;
+      break;
+  }
+  return true;
 };
